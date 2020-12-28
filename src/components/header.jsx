349478b99cfb5493f0fe3ef3ef44b1ff668af878
Ilya <dert_94@mail.ru>
2020-12-28 09:24:53 +0300

    Add new project + normalize +  remove unnecessary

---
+++ src/components/header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header className="App-header">
+            <title className="header__title">АлкоХэлпер</title>
+    </header>
+    );
+};
+export default Header;
