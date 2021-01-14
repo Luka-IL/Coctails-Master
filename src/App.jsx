2d4bcf27b9562aa6afb2fd9252101b8bdff2a30b
Ilya <dert_94@mail.ru>
2021-01-14 08:53:42 +0300

    Header and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Header from "./components/header"
+import "materialize-css/dist/css/materialize.min.css"
+import "../node_modules/normalize.css/normalize.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      
     </div>
   );
 };
